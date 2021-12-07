5554321c3387d9b14b73d18d59f8a16e935d284d
liyuli <liyuli@bevol.cn>
2021-12-07 18:43:28 +0800

    eureka的试用补充

---
+++ cloud-eureka-server7001/src/main/java/com.springcloud/CloudEurekaServer7001Application.java
@@ -0,0 +1,11 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEurekaServer7001Application {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaServer7001Application.class,args);
+    }
+}
